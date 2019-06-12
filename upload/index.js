29df343fb59de38ac8ef83eb1833db21ac9595fc
Rebecca Bob <rebecca.rupel@gmail.com>
2019-06-12 16:10:46 -0700

    add multer and POST to upload

---
+++ upload/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({dest: __dirname + '/uploads/images'});
+
+const app = express();
+
+
+//app.use(express.static('public'));
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+    if(req.file) {
+        res.json(req.file);
+    }
+    else throw 'error';
+});
