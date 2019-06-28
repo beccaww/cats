714c2b894a9d5426bfff4e07505a348acf51f133
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2019-06-27 20:04:48 -0400

    overview

--- images/router.js
+++ images/router.js
@@ -11,9 +11,9 @@ const router = express.Router();
 
 router.post('/', upload.single('imageField'), (req, res) => {
     if(req.file) {
-        console.error(req.file);
         const i = new Image({
-            data: req.file.buffer
+            data: req.file.buffer,
+            user: req.user
         });
         i.save().then(() => {
             return res.status(201).json({
@@ -29,5 +29,48 @@ router.post('/', upload.single('imageField'), (req, res) => {
     }
 });
 
+router.get('/', (req, res) => {
+    Image.findByUserId(req.user._id).then()
+    // return a json list
+    // [ { _id: asdlkfj }, { _id: alskdjfa }, ....]
+});
 
-module.exports = router ; 
+router.get('/:id', (req, res) => {
+    Image.findById(req.params.id).then(record => res.send(record.data));
+});
+
+
+// Story:
+// When you log in, you want to see all your images
+
+// Corollaries
+// A   - be able to search for images based on your user id
+// A01 - you have to store the user id with each image when uploading
+// B   - add endpoint to retrieve image (data) based on image id
+
+
+/*
+__________
+|         |
+| server   
+|_________|
+
+__________
+|         |
+| browser   
+|_________|
+
+
+After logging in:
+- browser to fetch list of images that you have uploaded
+-- there needs to be an api endpoint to support this - see Corollary A
+- from that list, we have to generate the html that references the images (ex: <img src="/api/images/alskdjf" />)
+- Corollary B - the API needs an endpoint to retrieve the image based on the ids
+
+- FE needs a page that allows for uploading images - see corollary A01
+
+
+
+*/
+
+module.exports = router;
